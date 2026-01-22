caa5f93cabcd805337a7aa75cda5742de100884b
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-01-21 23:08:17 -0300

    feat: adicionando imagens

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
 import './App.css'
+import Play from './pages/Play'
 
 function App() {
   const [playerScore, setPlayerScore] = useState(0)
   const [opponentScore, setOpponentScore] = useState(0)
   return (
-    <Header playerScore={playerScore} opponentScore={opponentScore} />
+    <div className="container mx-auto p-4">
+      <Header playerScore={playerScore} opponentScore={opponentScore} />
+      <Play />
+    </div>
+
+
   )
 }
 
